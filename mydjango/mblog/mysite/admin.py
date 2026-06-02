e333ccd14ae9cab1fdab7bc7e908b78099288be9
young42344234-cell <young42344234@gmail.com>
2026-06-02 15:20:06 +0800

    Add files via upload

---
+++ mydjango/mblog/mysite/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from mysite.models import Post
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'slug', 'pub_date')
+
+
+admin.site.register(Post, PostAdmin)
+
+# Register your models here.
---
+++ mydjango/mblog/mysite/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from mysite.models import Post
+from datetime import datetime
+
+
+# def homepage(request):
+#     posts = Post.objects.all()
+#     post_lists = list()
+#     for count, post in enumerate(posts):
+#         post_lists.append("No.{}:".format(str(count)) + str(post)+"<br>") 
+#     return HttpResponse(post_lists)
+def homepage(request):
+    posts = Post.objects.all()
+    now = datetime.now()
+    return render(request, 'index.html', locals())
+
+def showpost(request, slug):
+	try:
+		post = Post.objects.get(slug = slug)
+		if post != None:
+			return render(request, 'post.html', locals())
+	except:
+		return redirect('/')
+
